00c769bee4800e2e863a98d25af67096d29c7f81
Uğur Baltacı <me@ugurbaltaci.com>
2014-12-11 00:54:22 +0200

    initial

---
+++ index.js
@@ -0,0 +1,60 @@
+var Hapi = require("hapi");
+
+var server = new Hapi.Server("localhost", "3001", {
+    security: true,
+    validation: {
+        allowUnknown: true
+    },
+    state: {
+        cookies: {
+            clearInvalid: true,
+            strictHeader: false,
+            failAction: "log"
+        }
+    },
+    cors: {
+        origin: [
+            "localhost",
+            "*.mallframe.com",
+            "*.franciacortaoutlet.it"
+        ]
+    }
+});
+
+server.pack.register({
+    plugin: require("yar"),
+    options: {
+        maxCookieSize: 0,
+        cookieOptions: {isSecure: false, password: "some_secret_key"}
+    }
+}, function (err) {
+    if(!err) {
+        server.route({
+            method: "GET",
+            path: "/test",
+            config: {
+                handler: function (request, reply) {
+                    request.session.flash("test", "some flash message");
+                    reply.redirect("/test2");
+                }
+            }
+        });
+        server.route({
+            method: "GET",
+            path: "/test2",
+            config: {
+                handler: function (request, reply) {
+                    var flash_msg = request.session.flash("test");
+                    // flash_msg empty array.
+                    reply(flash_msg);
+                }
+            }
+        });
+
+        server.start(function(err) {
+            if (!err) {
+                console.log("server started @3001");
+            }
+        });
+    }
+});
